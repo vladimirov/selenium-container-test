3fe5dd2144caf6eccca8a71f19e0e199dc77b628
vladimirov <denis.vladimiroff@gmail.com>
2018-10-31 19:26:41 +0200

    added video recording

---
+++ src/test/java/Listener.java
@@ -0,0 +1,34 @@
+import org.testng.ITestContext;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+
+import java.io.File;
+
+public abstract class Listener extends RecordNameTest implements ITestListener {
+
+    File recordingDir;
+
+
+    private static String getTestMethodName(ITestResult iTestResult) {
+        return iTestResult.getMethod().getConstructorOrMethod().getName();
+    }
+
+    @Override
+    public void onTestSuccess(ITestResult iTestResult) {
+        recordingDir = new File("target/PASSED-" + System.currentTimeMillis());
+        System.out.println("I am in onTestSuccess method " +  getTestMethodName(iTestResult) + " succeed");
+    }
+
+    @Override
+    public void onTestFailure(ITestResult iTestResult) {
+        recordingDir = new File("target/FAILED-" + System.currentTimeMillis());
+        System.out.println("I am in onTestFailure method " +  getTestMethodName(iTestResult) + " failed");
+    }
+
+    @Override
+    public void onTestSkipped(ITestResult iTestResult) {
+        System.out.println("I am in onTestSkipped method "+  getTestMethodName(iTestResult) + " skipped");
+    }
+
+
+}
